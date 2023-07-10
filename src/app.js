5596de4966362f3a0ee51070ca6b4b86718553ae
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2023-07-10 15:12:12 -0300

    [feat]: middlewares and initializing app in sever

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 // import mongoose from 'mongoose';
-// import cors from 'cors';
-// import path from 'path';
+import cors from 'cors';
+import path from 'path';
 import routes from './routes';
 
 class App {
@@ -17,14 +17,14 @@ class App {
   }
 
   middlewares() {
-    // this.server.use(cors());
+    this.server.use(cors());
 
-    // this.server.use(
-    //     '/files',
-    //     express.static(path.resolve(__dirname, '..', 'uploads')),
-    // );
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'uploads')),
+    );
 
-    // this.server.use(express.json());
+    this.server.use(express.json());
   }
 
   routes() {
