95ab31af0d62738d44c53e1ec7ae5d1983f65a1d
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2023-07-12 12:41:17 -0300

    [feat]: CRUD profile controller and routes of profile

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,18 @@ import multer from 'multer';
 import uploadConfig from './config/upload';
 
 import SessionController from './controllers/SessionController';
+import ProfileController from './controllers/ProfileController';
 import ProductsController from './controllers/ProductsController';
 
 const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.get('/sessions', SessionController.store);
+
+routes.get('/profile', ProfileController.index);
+routes.post('/profile', ProfileController.store);
+routes.put('/profile/:user_id', ProfileController.update);
+routes.delete('/profile', ProfileController.destroy);
 
 routes.get('/products', ProductsController.index);
 routes.post('/products', upload.single('image'), ProductsController.store);
