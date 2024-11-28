5192e85936252a074ce7a4610a6ac75492b40e28
Camila Ribeiro Pedroso <camilaribeiropedroso@MacBook-Pro-de-Camila.local>
2024-11-28 17:37:19 -0300

    <bug>[DB]: refactor connection DB mongoose 7.0

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import express from 'express';
 import mongoose from 'mongoose';
+import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import routes from './routes';
 
 class App {
   constructor() {
+    this.connectionDB();
     this.server = express();
     this.middlewares();
     this.routes();
+  }
 
-    // ip database 186.224.135.20/32
-    mongoose.connect('mongodb+srv://luzvioleta:violeta@luzvioleta.h2xeiso.mongodb.net/luzvioleta', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  async connectionDB() {
+    const uri = 'mongodb+srv://luzvioleta:violeta@luzvioleta.h2xeiso.mongodb.net/luzvioleta';
+    const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+    try {
+      await mongoose.connect(uri, clientOptions, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    } catch (erro) {
+      // eslint-disable-next-line no-console
+      console.log(erro);
+    }
   }
 
   middlewares() {
