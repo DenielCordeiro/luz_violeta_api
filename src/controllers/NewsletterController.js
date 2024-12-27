6af491b93bd1d09269e56d03bdf23f8af3eb41b4
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2024-12-27 12:23:06 -0300

    <feat>[Newsletter]: Error message in functions and completed CRUD in Newsletter

--- src/controllers/NewsletterController.js
+++ src/controllers/NewsletterController.js
@@ -3,7 +3,17 @@ import News from '../models/News';
 
 class NewsletterControlle {
   async index(req, res) {
-    return res.json({ ok: true });
+    const { allNews } = req.params;
+
+    try {
+      const data = await News.find({ allNews });
+      return res.json({ data });
+    } catch (error) {
+      return res.status(400).json({
+        fail: 'Não foi possível buscar todas as imagens!',
+        messageError: error,
+      });
+    }
   }
 
   async store(req, res) {
@@ -25,29 +35,92 @@ class NewsletterControlle {
     } = req.file ? req.file : '';
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ erro: 'Falha na validação dos campos!' });
+      return res.status(400).json({ error: 'Falha na validação dos campos!' });
     }
 
-    const news = await News.create({
-      type,
-      linkProduct,
-      file: {
-        name: nameImage,
-        size: sizeImage,
-        key: keyImage,
-        url: urlImage,
-      },
-    });
+    try {
+      const news = await News.create({
+        type,
+        linkProduct,
+        file: {
+          name: nameImage,
+          size: sizeImage,
+          key: keyImage,
+          url: urlImage,
+        },
+      });
 
-    return res.json({ data: news });
+      return res.json({ data: news });
+    } catch (error) {
+      return res.status(400).json({
+        fail: 'Não foi possivel criar uma nova imagem!',
+        messageError: error,
+      });
+    }
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const schema = Yup.object().shape({
+      type: Yup.string().required(),
+      linkProduct: Yup.string(),
+    });
+
+    const {
+      type,
+      linkProduct,
+    } = req.body;
+
+    const { news_id } = req.params;
+
+    const {
+      originalname: nameImage,
+      size: sizeImage,
+      filename: keyImage,
+      firebaseUrl: urlImage,
+    } = req.file ? req.file : '';
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação dos campos!' });
+    }
+
+    try {
+      await News.updateOne({ _id: news_id }, {
+        type,
+        linkProduct,
+        file: {
+          name: nameImage,
+          size: sizeImage,
+          key: keyImage,
+          url: urlImage,
+        },
+      });
+
+      return res.send();
+    } catch (error) {
+      return res.status(400).json({
+        fail: 'Não foi possível atualizar a dados da imagem!',
+        messageError: error,
+      });
+    }
   }
 
   async destroy(req, res) {
-    return res.json({ ok: true });
+    const { news_id } = req.params;
+
+    if (news_id === null || news_id === undefined) {
+      return res.status(400).json({ error: 'Não foi possível encontrar o id da imagem!' });
+    }
+
+    try {
+      const result = await News.findByIdAndDelete({ _id: news_id });
+
+      return res.json({ result });
+    } catch (error) {
+      return res.status(400).json({
+        fail: 'Não foi possível deletar a imagem!',
+        messageError: error,
+      });
+    }
   }
 }
 
