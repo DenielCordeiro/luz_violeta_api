6148bf98e163a3be4efe43621c5a0044cc5e7a42
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2025-12-13 18:52:44 -0300

    refactor: server to start in render

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
-import app from './app';
+import app from './app.js';
 
-app.listen(3333);
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
