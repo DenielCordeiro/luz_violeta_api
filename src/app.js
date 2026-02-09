6973461d01ba0a84461aff3e9729ef10bfa0ed87
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2026-02-09 19:06:06 -0300

    refactor[APP]: remove startign with function PIX

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ class App {
 
     this.middlewares();
     this.routes();
-    this.authenticateEFIBank();
     this.connectionDB();
   }
 
@@ -23,12 +22,6 @@ class App {
 
     this.server.listen(PORT, async () => {
       console.log(`🚀 Servidor rodando na porta ${PORT}`);
-
-      try {
-        await EFIWebhook.registerWebhook();
-      } catch (err) {
-        console.error('Erro ao registrar webhook:', err.message);
-      }
     });
   }
 
