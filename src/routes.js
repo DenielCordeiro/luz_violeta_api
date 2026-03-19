c65feda0a97b1b349c7c35d3e44b630a2f7be2ff
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2026-03-18 21:57:58 -0300

    refactor[Freight]: using axios to requests and dotenv to save data

--- src/routes.js
+++ src/routes.js
@@ -12,8 +12,8 @@ import ReviewController from './controllers/ReviewController.js';
 import ProductsController from './controllers/ProductsController.js';
 import CartController from './controllers/CartController.js';
 import AboutController from './controllers/AboutController.js';
-import MelhorEnvioController from './controllers/MelhorEnvioController.js';
 import PixController from './controllers/PixController.js';
+import FreightController from './controllers/FreightController.js';
 
 const routes = new Router();
 
@@ -53,7 +53,7 @@ routes.put('/buy_product', CartController.buyProduct);
 routes.get('/about', AboutController.getAbout);
 routes.put('/about', AboutController.updateAbout);
 
-routes.post('/melhor-envio/:postal_code', MelhorEnvioController.searchPostalCode);
+routes.post('/melhor-envio/:zipCode', FreightController.calculateFreight);
 
 routes.get('/payments/charges', PixController.getCharges);
 routes.post('/payments/pix', PixController.getPIX);
